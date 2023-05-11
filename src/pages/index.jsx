f2d8d4552a03fce75ecbd9ab5cd98769b1173175
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-05-11 17:42:33 +0900

    ✨ ログイン機能作成

---
+++ src/pages/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Head from 'next/head'
+import { useSession } from 'next-auth/react'
+import Login from '../components/Login'
+import Logout from '../components/Logout'
+// import DeleteUser from '../components/deleteUser'
+
+export default function Home() {
+  const { data: session, status } = useSession()
+
+  return (
+    <>
+      <Head>
+        <title>next-rails-google-auth</title>
+      </Head>
+      <div>
+        <h1>next-rails-google-auth</h1>
+
+        {status === 'authenticated' ? (
+          <div>
+            <p>セッションの期限：{session.expires}</p>
+            <p>ようこそ、{session.user.name}さん</p>
+            <img src={session.user.image} alt="" style={{ borderRadius: '50px' }} />
+            <div>
+              <Logout />
+            </div>
+            {/* <div>
+              <DeleteUser />
+            </div> */}
+          </div>
+        ) : (
+          <Login />
+        )}
+      </div>
+    </>
+  )
+}
